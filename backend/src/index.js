cd15ec4299767e890b6f45f5b48e0859d189c686
intern-bootway <intern@bootway.in>
2026-04-06 14:21:37 +0530

    fix error

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,12 @@ import invoiceRoutes from './routes/invoices.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://logiedge-frontend.vercel.app'
+  ]
+}));
 app.use(express.json());
 
 app.use('/api/customers', customerRoutes);
